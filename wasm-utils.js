53ce6315ccdefc6876c36110d7cfd617d564ea96
chrisdavidmills <chrisdavidmills@gmail.com>
2018-02-28 20:10:24 +0000

    removing unneeded fetch and instantiate utility function

--- wasm-utils.js
+++ wasm-utils.js
@@ -1,19 +1,4 @@
-// 1. +++ fetchAndInstantiate() +++ //
-
-// This library function fetches the wasm module at 'url', instantiates it with
-// the given 'importObject', and returns the instantiated object instance
-
-function fetchAndInstantiate(url, importObject) {
-  return fetch(url).then(response =>
-    response.arrayBuffer()
-  ).then(bytes =>
-    WebAssembly.instantiate(bytes, importObject)
-  ).then(results =>
-    results.instance
-  );
-}
-
-// 2. +++ instantiateCachedURL() +++ //
+// 1. +++ instantiateCachedURL() +++ //
 
 // This library function fetches the wasm Module at 'url', instantiates it with
 // the given 'importObject', and returns a Promise resolving to the finished
