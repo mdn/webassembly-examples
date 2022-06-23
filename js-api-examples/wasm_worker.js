64441f91a64007611751979829e30382f530d3e3
Garcia <90156453+Phosra@users.noreply.github.com>
2022-06-22 17:20:11 -0700

    Update wasm_worker.js

--- js-api-examples/wasm_worker.js
+++ js-api-examples/wasm_worker.js
@@ -6,7 +6,7 @@ const importObject = {
   }
 };
 
-window.onmessage = function(event) {
+self.onmessage = function(event) {
   console.log("module received from main thread");
   const module = event.data;
 
